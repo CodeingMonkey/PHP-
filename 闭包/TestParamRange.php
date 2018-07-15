27c9f0b828a299e0714013e39b9dc8e3333d256f
rongsheng <360996527@qq.com>
2018-07-15 21:39:24 +0800

    闭包

---
+++ 闭包/TestParamRange.php
@@ -0,0 +1,31 @@
+<?php
+
+$message = 'debug';
+
+/**
+ * 没有 "use"的话程序会提示错误
+ */
+$example = function () {
+    echo $message;
+};
+echo $example();
+echo '<br>';
+
+/**
+ * 闭包可以从父作用于（可以理解是闭包之前的代码）继承变量，并且在闭包中使用
+ * 但是使用这样的变量，必须使用use语言结构将变量传递到闭包函数中
+ */
+$example = function () use ($message) {
+    var_dump($message);
+};
+echo $example();
+echo '<br>';
+
+/**
+ * @param $arg
+ * 闭包同样可以接收参数
+ */
+$example = function ($arg) use ($message) {
+    var_dump($arg . ' ' . $message);
+};
+$example("hello");
